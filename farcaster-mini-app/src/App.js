f6322aec24addd95de715937fccad9646871f9de
Erdal ŞAHAN <dhm23198@dhmi.root>
2025-10-08 09:51:48 +0300

    Initial commit

--- farcaster-mini-app/src/App.js
+++ farcaster-mini-app/src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import React, { useEffect } from "react";
 import { FrameSDK } from "@farcaster/frame-sdk";
-import { sdk } from '@farcaster/miniapp-sdk'
+import { sdk as miniSdk } from '@farcaster/miniapp-sdk';
 import WalletConnect from './components/WalletConnect'
 import GM from './components/GM'
 function App() {
-  useEffect(() => {
-  const sdk = new FrameSDK();
-  sdk.actions.ready(); // Farcaster’a “ben hazırım” sinyali gönder
-}, []);
-useEffect(() => {
-   sdk.actions.ready()
-}, [])
-
+//   useEffect(() => {
+//   const sdk = new FrameSDK();
+//   sdk.actions.ready(); // Farcaster’a “ben hazırım” sinyali gönder
+// }, []);
+// useEffect(() => {
+//    sdk.actions.ready()
+// }, [])
+ useEffect(() => {
+    const frame = new FrameSDK();
+    frame.actions.ready();   // Frame SDK için hazır sinyali
+    miniSdk.actions.ready(); // Mini app SDK için hazır sinyali (isteğe bağlı)
+  }, []);
   return (
     <div className='App'>
       <WalletConnect/>
