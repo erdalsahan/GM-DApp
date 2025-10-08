6f3ddb551bd4c8ff47fb6249b2d8fab001f37d49
Erdal ŞAHAN <dhm23198@dhmi.root>
2025-10-08 11:00:36 +0300

    Initial commit

--- farcaster-mini-app/src/App.js
+++ farcaster-mini-app/src/App.js
@@ -5,21 +5,16 @@ import { sdk } from '@farcaster/miniapp-sdk';
 import WalletConnect from './components/WalletConnect'
 import GM from './components/GM'
 function App() {
-//   useEffect(() => {
-//   const sdk = new FrameSDK();
-//   sdk.actions.ready(); // Farcaster’a “ben hazırım” sinyali gönder
-// }, []);
-// useEffect(() => {
-//    sdk.actions.ready()
-// }, [])
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (sdk && sdk.actions && sdk.actions.ready) {
-        sdk.actions.ready();
-        clearInterval(interval);
-      }
-    }, 50); // her 50ms kontrol et
-  }, []);
+  const callReady = async () => {
+    while (!sdk || !sdk.actions || !sdk.actions.ready) {
+      await new Promise(res => setTimeout(res, 50));
+    }
+    sdk.actions.ready();
+  };
+  callReady();
+}, []);
   return (
     <div className='App'>
       <WalletConnect/>
